521378bed6ba62e20baa963b90b8671dfd527760
BlaBlaBlaWang <wjx1999126@qq.com>
2021-10-15 21:02:24 +0800

    start!

---
+++ build/unitest/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /home/wjx/ob/unitest
+# Build directory: /home/wjx/ob/build/unitest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/wjx/ob/build/unitest/bitmap_test[1]_include.cmake")
+include("/home/wjx/ob/build/unitest/bp_manager_test[1]_include.cmake")
+include("/home/wjx/ob/build/unitest/log_test[1]_include.cmake")
+include("/home/wjx/ob/build/unitest/md5_test[1]_include.cmake")
+include("/home/wjx/ob/build/unitest/path_test[1]_include.cmake")
+include("/home/wjx/ob/build/unitest/pidfile_test[1]_include.cmake")
+include("/home/wjx/ob/build/unitest/rc_test[1]_include.cmake")
